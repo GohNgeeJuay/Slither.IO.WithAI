2154a464a53d7e7c4fa2834b9df7a177e7e274eb
Juay <ngoh0002@student.monash.edu>
2022-01-13 11:24:57 +0800

    Move all of the global vars into __main__ Have error cannot pickle surface object

--- main.py
+++ main.py
@@ -9,31 +9,33 @@ class GameMode:
         self.mode = 0
 
 class Snake:    
-    def __init__(self,num):
+    def __init__(self,num,cellSize,blueColor,orangeColor,screen):
+        self.cellSize = cellSize
+        self.screen = screen
         if num == 1:
             self.body = [Vector2(5,10),Vector2(4,10),Vector2(3,10)]        
             self.direction = Vector2(0,0)
             self.moving = False
             self.new_block = False
-            self.color = BLUESNAKECOLOR
+            self.color = blueColor
             
             headUp = pygame.image.load('Graphics\BlueSnakeUp.png').convert_alpha()
-            headUp = pygame.transform.scale(headUp,(CELLSIZE,CELLSIZE))
+            headUp = pygame.transform.scale(headUp,(cellSize,cellSize))
             self.headUpStr = pygame.image.tostring(headUp,"RGB")
             self.headUpSize = headUp.get_size()
 
             headDown = pygame.image.load('Graphics\BlueSnakeDown.png').convert_alpha()
-            headDown = pygame.transform.scale(headDown,(CELLSIZE,CELLSIZE))
+            headDown = pygame.transform.scale(headDown,(cellSize,cellSize))
             self.headDownStr = pygame.image.tostring(headDown,"RGB")
             self.headDownSize = headDown.get_size()
 
             headLeft = pygame.image.load('Graphics\BlueSnakeLeft.png').convert_alpha()
-            headLeft = pygame.transform.scale(headLeft,(CELLSIZE,CELLSIZE))
+            headLeft = pygame.transform.scale(headLeft,(cellSize,cellSize))
             self.headLeftStr = pygame.image.tostring(headLeft,"RGB")
             self.headLeftSize = headLeft.get_size()
 
             headRight = pygame.image.load('Graphics\BlueSnakeRight.png').convert_alpha()
-            headRight = pygame.transform.scale(headRight,(CELLSIZE,CELLSIZE))
+            headRight = pygame.transform.scale(headRight,(cellSize,cellSize))
             self.headRightStr = pygame.image.tostring(headRight,"RGB")
             self.headRightSize = headRight.get_size()
 
@@ -42,25 +44,25 @@ class Snake:
             self.direction = Vector2(0,0)
             self.moving = False
             self.new_block = False
-            self.color = ORANGESNAKECOLOR
+            self.color = orangeColor
 
             headUp = pygame.image.load('Graphics\OrangeSnakeUp.png').convert_alpha()
-            headUp = pygame.transform.scale(headUp,(CELLSIZE,CELLSIZE))
+            headUp = pygame.transform.scale(headUp,(cellSize,cellSize))
             self.headUpStr = pygame.image.tostring(headUp,"RGB")
             self.headUpSize = headUp.get_size()
 
             headDown = pygame.image.load('Graphics\OrangeSnakeDown.png').convert_alpha()
-            headDown = pygame.transform.scale(headDown,(CELLSIZE,CELLSIZE))
+            headDown = pygame.transform.scale(headDown,(cellSize,cellSize))
             self.headDownStr = pygame.image.tostring(headDown,"RGB")
             self.headDownSize = headDown.get_size()
 
             headLeft = pygame.image.load('Graphics\OrangeSnakeLeft.png').convert_alpha()
-            headLeft = pygame.transform.scale(headLeft,(CELLSIZE,CELLSIZE))
+            headLeft = pygame.transform.scale(headLeft,(cellSize,cellSize))
             self.headLeftStr = pygame.image.tostring(headLeft,"RGB")
             self.headLeftSize = headLeft.get_size()
 
             headRight = pygame.image.load('Graphics\OrangeSnakeRight.png').convert_alpha()
-            headRight = pygame.transform.scale(headRight,(CELLSIZE,CELLSIZE))  
+            headRight = pygame.transform.scale(headRight,(cellSize,cellSize))  
             self.headRightStr = pygame.image.tostring(headRight,"RGB")
             self.headRightSize = headRight.get_size()
 
@@ -103,37 +105,45 @@ class Snake:
         self.update_head_graphics()
         for index, block in enumerate(self.body):
             #create a rect
-            snakeRect = pygame.Rect(int(block.x * CELLSIZE), int(block.y *CELLSIZE),CELLSIZE,CELLSIZE)
+            snakeRect = pygame.Rect(int(block.x * self.cellSize), int(block.y *self.cellSize),self.cellSize,self.cellSize)
             if index == 0: #if it is the head, create head object
-                screen.blit(pygame.image.fromstring(self.headStr,self.headSize,"RGB"),snakeRect)
+                self.screen.blit(pygame.image.fromstring(self.headStr,self.headSize,"RGB"),snakeRect)
                 #screen.blit(self.head,snakeRect)
             else:   #if not head, just use a rectangle
-                pygame.draw.rect(screen, self.color,snakeRect)
+                pygame.draw.rect(self.screen, self.color,snakeRect)
+
 class Fruit:    
-    def __init__(self, apple: pygame.Surface) -> None:
-        self.randomize()
+    def __init__(self, apple: pygame.Surface, cellNum, cellSize,screen) -> None:
+        self.randomize(cellNum)
         self.appleStr = pygame.image.tostring(apple,"RGB")
         self.appleSize = apple.get_size()
+        self.cellNum = cellNum
+        self.cellSize = cellSize
+        self.screen = screen
 
-    def randomize(self):
+    def randomize(self,cellNum):
         #Must -1 because its the top left corner of rectangle
-        self.x = random.randint(0,CELLNUMBER-1)
-        self.y = random.randint(0,CELLNUMBER-1)
+        self.x = random.randint(0,cellNum-1)
+        self.y = random.randint(0,cellNum-1)
         self.pos = Vector2(self.x,self.y) #Put into the Vector2
 
     def draw_fruit(self):
         #create rect
-        fruitRect = pygame.Rect(int(self.pos.x * CELLSIZE), int(self.pos.y *CELLSIZE),CELLSIZE,CELLSIZE)
-        screen.blit(pygame.image.fromstring(self.appleStr,self.appleSize,"RGB"),fruitRect)
-        #draw rect
-        #screen.blit(self.apple,fruitRect)
+        fruitRect = pygame.Rect(int(self.pos.x * self.cellSize), int(self.pos.y *self.cellSize),self.cellSize,self.cellSize)
+        self.screen.blit(pygame.image.fromstring(self.appleStr,self.appleSize,"RGB"),fruitRect)
 
 class Main:
    
-    def __init__(self, gameMode,apple1: pygame.Surface,apple2: pygame.Surface):
-        self.snakes = [Snake(1),Snake(2)]
-        self.fruits = [Fruit(apple1), Fruit(apple2)]
+    def __init__(self, gameMode,apple1: pygame.Surface,apple2: pygame.Surface, font,cellSize,cellNum,blueColor,orangeColor,screen):
+        self.snakes = [Snake(1,cellSize,blueColor,orangeColor,screen),Snake(2,cellSize,blueColor,orangeColor,screen)]
+        self.fruits = [Fruit(apple1,cellNum,cellSize,screen), Fruit(apple2,cellNum,cellSize,screen)]
         self.mode = gameMode
+        self.font = font
+        self.cellSize = cellSize
+        self.cellNum = cellNum
+        self.blueColor = blueColor
+        self.orangeColor = orangeColor
+        self.screen = screen
 
     def update(self):
         self.check_fail()
@@ -161,7 +171,7 @@ class Main:
             p3.join()
             p4.join()
 
-        #TODO: snake 1 -> fruit 1 -> snake 2 -> fruit 2. Priority of displaying in ascen ding order. 
+        #TODO: snake 1 -> fruit 1 -> snake 2 -> fruit 2. Priority of displaying in ascending order. 
         # Might have to fix using multithreading
         # for i in range(2):    
         #     draw_snake(self.snakes[i])
@@ -172,19 +182,19 @@ class Main:
             if self.fruits[i].pos == self.snakes[i].body[0]:
                 #if the head eats a fruit
                 #reposition fruit
-                self.fruits[i].randomize()
+                self.fruits[i].randomize(self.cellNum)
                 #add another block to snake
                 self.snakes[i].add_block()
 
                 #ensure that the fruit does not be on the snake
                 for block in self.snakes[i].body[1:]:
                     if block == self.fruits[i].pos:
-                        self.fruits[i].randomize()
+                        self.fruits[i].randomize(self.cellNum)
 
     def check_fail(self):
         for index,snake in enumerate(self.snakes):
             #check out of bounds
-            if not 0 <= snake.body[0].x < CELLNUMBER or not 0 <= snake.body[0].y < CELLNUMBER:
+            if not 0 <= snake.body[0].x < self.cellNum or not 0 <= snake.body[0].y < self.cellNum:
                 if index == 0:
                     self.show_game_over(2)
                 else:
@@ -210,12 +220,12 @@ class Main:
         elif winner == 2:
             gameOverText = "Player 2 Wins!"
         
-        gameOverSurface1 = gameFont.render(gameOverText,True,(0,0,0))
-        gameOverSurface2 = gameFont.render("Press the return key to return to menu",True,(0,0,0))
-        gameOverRect1 = gameOverSurface1.get_rect(center = ( (CELLSIZE*CELLNUMBER/2),(CELLSIZE*CELLNUMBER/2)))
+        gameOverSurface1 = self.font.render(gameOverText,True,(0,0,0))
+        gameOverSurface2 = self.font.render("Press the return key to return to menu",True,(0,0,0))
+        gameOverRect1 = gameOverSurface1.get_rect(center = ( (self.cellSize*self.cellNum/2),(self.cellSize*self.cellNum/2)))
         gameOverRect2 = gameOverSurface2.get_rect(center = ( gameOverRect1.centerx,gameOverRect1.bottom+10))
-        screen.blit(gameOverSurface1,gameOverRect1) 
-        screen.blit(gameOverSurface2,gameOverRect2) 
+        self.screen.blit(gameOverSurface1,gameOverRect1) 
+        self.screen.blit(gameOverSurface2,gameOverRect2) 
 
 
         pygame.display.flip()
@@ -232,32 +242,32 @@ class Main:
 
     def draw_grass(self):
         grassColor = (167,209,61)
-        for row in range(CELLNUMBER):
+        for row in range(self.cellNum):
             if row % 2 == 0:
-                for col in range(CELLNUMBER):
+                for col in range(self.cellNum):
                     if col % 2 == 0:
-                        grassRect = pygame.Rect(col*CELLSIZE,row*CELLSIZE,CELLSIZE,CELLSIZE)
-                        pygame.draw.rect(screen,grassColor,grassRect)
+                        grassRect = pygame.Rect(col*self.cellSize,row*self.cellSize,self.cellSize,self.cellSize)
+                        pygame.draw.rect(self.screen,grassColor,grassRect)
 
             else:
-                for col in range(CELLNUMBER):
+                for col in range(self.cellNum):
                     if col % 2 != 0:
-                        grassRect = pygame.Rect(col*CELLSIZE,row*CELLSIZE,CELLSIZE,CELLSIZE)
-                        pygame.draw.rect(screen,grassColor,grassRect)
+                        grassRect = pygame.Rect(col*self.cellSize,row*self.cellSize,self.cellSize,self.cellSize)
+                        pygame.draw.rect(self.screen,grassColor,grassRect)
 
     def draw_score(self):
 
         #Score surface for snake 1
         scoreText1 = str(len(self.snakes[0].body) - 3)
-        scoreSurface1 = gameFont.render(scoreText1,True,(56,74,12))
+        scoreSurface1 = self.font.render(scoreText1,True,(56,74,12))
 
         #Score surface for snake 2
         scoreText2 = str(len(self.snakes[1].body) - 3)
-        scoreSurface2 = gameFont.render(scoreText2,True,(56,74,12))
+        scoreSurface2 = self.font.render(scoreText2,True,(56,74,12))
 
         #Position for the score snake 1
-        scoreX = int(CELLSIZE*CELLNUMBER-60)
-        scoreY = int(CELLSIZE*CELLNUMBER-40)
+        scoreX = int(self.cellSize*self.cellNum-60)
+        scoreY = int(self.cellSize*self.cellNum-40)
 
         scoreRect1 = scoreSurface1.get_rect(center = (scoreX,scoreY))
         apple = pygame.image.fromstring(self.appleStr,self.appleSize,"RGB")
@@ -269,11 +279,11 @@ class Main:
         appleRect2 = self.fruits[1].apple.get_rect(midright = (scoreRect2.left,scoreRect2.centery))
 
         pygame.draw.rect(screen, (167,209,61),bgRect1)
-        screen.blit(scoreSurface1,scoreRect1)            #Display score
-        screen.blit(scoreSurface2,scoreRect2)            
-        screen.blit(self.fruits[0].apple,appleRect1)     #Display an apple
-        screen.blit(self.fruits[1].apple,appleRect2)     
-        pygame.draw.rect(screen, (56,74,12),bgRect1,2)   #For border
+        self.screen.blit(scoreSurface1,scoreRect1)            #Display score
+        self.screen.blit(scoreSurface2,scoreRect2)            
+        self.screen.blit(self.fruits[0].apple,appleRect1)     #Display an apple
+        self.screen.blit(self.fruits[1].apple,appleRect2)     
+        pygame.draw.rect(self.screen, (56,74,12),bgRect1,2)   #For border
 
    
 #################################################START GAME################################################
@@ -281,8 +291,8 @@ class Main:
 def set_mode(value, index):
     gameMode.mode = index
 
-def start_the_game():
-    main_game = Main(gameMode.mode,apple1=blueApple,apple2=redApple)
+def start_the_game(gameMode,gameFont,CELLSIZE,CELLNUMBER,BLUESNAKECOLOR,ORANGESNAKECOLOR,screen,clock,blueApple,redApple):
+    main_game = Main(gameMode.mode,apple1=blueApple,apple2=redApple,font = gameFont,cellSize = CELLSIZE,cellNum = CELLNUMBER,blueColor = BLUESNAKECOLOR,orangeColor = ORANGESNAKECOLOR,screen = screen)
 
     while True:
         for event in pygame.event.get():
@@ -331,39 +341,35 @@ def start_the_game():
         clock.tick(60) #Cannot exceed 60 frames
 
 
-#init game
-os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (300,30)
-pygame.init()
-
-gameMode = GameMode()
-CELLSIZE = 20
-CELLNUMBER = 30
-BLUESNAKECOLOR = (35,200,250)
-ORANGESNAKECOLOR = (250,130,10)
-
-screen = pygame.display.set_mode((CELLNUMBER*CELLSIZE,CELLNUMBER*CELLSIZE))
-clock = pygame.time.Clock()
-blueApple = pygame.image.load('Graphics/blueApple.png').convert_alpha()
-blueApple = pygame.transform.scale(blueApple,(CELLSIZE,CELLSIZE))
-redApple = pygame.image.load('Graphics/redApple.png').convert_alpha()
-redApple = pygame.transform.scale(redApple,(CELLSIZE,CELLSIZE))
-
-gameFont = pygame.font.Font('Font\omegle\OMEGLE.ttf',25)
-
-SCREEN_UPDATE = pygame.USEREVENT
-pygame.time.set_timer(SCREEN_UPDATE,120)
-
-def show_menu():
+def show_menu(gameMode,gameFont,CELLSIZE,CELLNUMBER,BLUESNAKECOLOR,ORANGESNAKECOLOR,screen,clock,blueApple,redApple):
     menu = pygame_menu.Menu('Welcome to Slither', 400, 300, theme=pygame_menu.themes.THEME_GREEN)
     menu.add.selector('Mode :', [('2 players', 0),('VS Bot',1)], onchange= set_mode)
-    menu.add.button('Play', start_the_game)
+    menu.add.button('Play', start_the_game,gameMode,gameFont,CELLSIZE,CELLNUMBER,BLUESNAKECOLOR,ORANGESNAKECOLOR,screen,clock,blueApple,redApple)
     menu.add.button('Quit', pygame_menu.events.EXIT)
 
     #Call menu
     menu.mainloop(screen)
 
 if __name__ == '__main__':
-    show_menu()
+    #init game
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (300,30)
+    pygame.init()
+    gameMode = GameMode()
+    gameFont = pygame.font.Font('Font\omegle\OMEGLE.ttf',25)
+    CELLSIZE = 20
+    CELLNUMBER = 30
+    BLUESNAKECOLOR = (35,200,250)
+    ORANGESNAKECOLOR = (250,130,10)
+    screen = pygame.display.set_mode((CELLNUMBER*CELLSIZE,CELLNUMBER*CELLSIZE))
+    clock = pygame.time.Clock()
+    blueApple = pygame.image.load('Graphics/blueApple.png').convert_alpha()
+    blueApple = pygame.transform.scale(blueApple,(CELLSIZE,CELLSIZE))
+    redApple = pygame.image.load('Graphics/redApple.png').convert_alpha()
+    redApple = pygame.transform.scale(redApple,(CELLSIZE,CELLSIZE))
+    SCREEN_UPDATE = pygame.USEREVENT
+    pygame.time.set_timer(SCREEN_UPDATE,120)
+
+    show_menu(gameMode,gameFont, CELLSIZE,CELLNUMBER,BLUESNAKECOLOR,ORANGESNAKECOLOR,screen,clock,blueApple,redApple)
 
 
 
